3e92f16e468ca1c04b2682b23c279c192ed6cb3a
Piotr Szydłowski <szydlowskiptr@gmail.com>
2021-02-08 21:30:03 +0100

    jenkis file , docker

--- src/test/java/tests/login/PositiveLoginTest.java
+++ src/test/java/tests/login/PositiveLoginTest.java
@@ -3,7 +3,7 @@ package tests.login;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import io.qameta.allure.TmsLink;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import page.objects.LoginPage;
 import tests.TestBase;
@@ -11,16 +11,17 @@ import utils.DriverUtils;
 import static navigation.ApplicationURLs.LOGIN_URL;
 
 public class PositiveLoginTest extends TestBase {
+    @Parameters("email")
     @Test
     @Description("Cel testu: Poprawne zalogowanie użytkownika")
     @Severity(SeverityLevel.BLOCKER)
-    public void loginUser(){
+    public void loginUser(String email){
         DriverUtils.navigateToPage(LOGIN_URL);
         final String FIRST_NAME = "JANUSZ";
         final String LAST_NAME = "KOWALSKI";
         LoginPage loginPage = new LoginPage();
         loginPage
-                .typeIntoFirstNameField("janusz.kowalski@gmail.com")
+                .typeIntoFirstNameField(email)
                 .typeIntoPasswordField("123456")
                 .clickLoginButton()
                 .assertThatGreentigMessageIsDisplayed("Hi, " + FIRST_NAME + " " + LAST_NAME);
